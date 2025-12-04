b314d68d6d84ee16593d551e6fc960b160186bc9
Nex <nexvlif@gmail.com>
2025-12-04 12:49:03 +0700

    feat: add 2 menu on dashboard

--- resources/views/school/pages/dashboard/dashboard.blade.php
+++ resources/views/school/pages/dashboard/dashboard.blade.php
@@ -113,6 +113,32 @@
                     <span class="d-none d-md-block ms-2 font-weight-medium">Guru</span>
                 </a>
             </li>
+            <li class="nav-item">
+                <a href="#" data-bs-toggle="tab"
+                    class="nav-link note-link d-flex align-items-center justify-content-center px-3 px-md-3 me-0 me-md-2 text-body-color"
+                    id="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 36 36">
+                        <path fill="currentColor"
+                            d="M16.43 16.69a7 7 0 1 1 7-7a7 7 0 0 1-7 7m0-11.92a5 5 0 1 0 5 5a5 5 0 0 0-5-5M22 17.9a25.4 25.4 0 0 0-16.12 1.67a4.06 4.06 0 0 0-2.31 3.68v5.95a1 1 0 1 0 2 0v-5.95a2 2 0 0 1 1.16-1.86a22.9 22.9 0 0 1 9.7-2.11a23.6 23.6 0 0 1 5.57.66Zm.14 9.51h6.14v1.4h-6.14z" />
+                        <path fill="currentColor"
+                            d="M33.17 21.47H28v2h4.17v8.37H18v-8.37h6.3v.42a1 1 0 0 0 2 0V20a1 1 0 0 0-2 0v1.47H17a1 1 0 0 0-1 1v10.37a1 1 0 0 0 1 1h16.17a1 1 0 0 0 1-1V22.47a1 1 0 0 0-1-1" />
+                    </svg>
+                    <span class="d-none d-md-block ms-2 font-weight-medium">Staff</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" data-bs-toggle="tab"
+                    class="nav-link note-link d-flex align-items-center justify-content-center px-3 px-md-3 me-0 me-md-2 text-body-color"
+                    id="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 36 36">
+                        <path fill="currentColor"
+                            d="M16.43 16.69a7 7 0 1 1 7-7a7 7 0 0 1-7 7m0-11.92a5 5 0 1 0 5 5a5 5 0 0 0-5-5M22 17.9a25.4 25.4 0 0 0-16.12 1.67a4.06 4.06 0 0 0-2.31 3.68v5.95a1 1 0 1 0 2 0v-5.95a2 2 0 0 1 1.16-1.86a22.9 22.9 0 0 1 9.7-2.11a23.6 23.6 0 0 1 5.57.66Zm.14 9.51h6.14v1.4h-6.14z" />
+                        <path fill="currentColor"
+                            d="M33.17 21.47H28v2h4.17v8.37H18v-8.37h6.3v.42a1 1 0 0 0 2 0V20a1 1 0 0 0-2 0v1.47H17a1 1 0 0 0-1 1v10.37a1 1 0 0 0 1 1h16.17a1 1 0 0 0 1-1V22.47a1 1 0 0 0-1-1" />
+                    </svg>
+                    <span class="d-none d-md-block ms-2 font-weight-medium">Pembina Extra</span>
+                </a>
+            </li>
         </div>
 
         <form class="mt-4 mt-md-0">
