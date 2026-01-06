b0ebe797d266473acab1fd051b7fc74bdd21daee
Friyn <friyn@users.noreply.github.com>
2026-01-06 08:08:46 +0700

    fix: Color slider jurnal staff

--- resources/views/staff/layouts/sidebar.blade.php
+++ resources/views/staff/layouts/sidebar.blade.php
@@ -144,7 +144,6 @@
                                         stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
                                         stroke-linejoin="round" />
                                 </svg>
-
                             </span>
                             <span class="hide-menu">Absensi</span>
                         </a>
